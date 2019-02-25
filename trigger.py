bf13559b289dbd9960faad985207cf8c1a3535c0
Logic <logic.lj@outlook.com>
2019-02-25 12:08:21 +0800

    <fix> add trigger

--- trigger.py
+++ trigger.py
@@ -1,22 +1,11 @@
-from base.engine import single_run, thread_run
-
-import scrapy_config
+import sys
 
 if __name__ == '__main__':
-    # single_run(scrapy_config.xxgx_thread)
-    # thread_run(scrapy_config.scjst_thread)
-    # single_run(scrapy_config.xxgx_thread)
-    # thread_run(scrapy_config.xxgx_thread)
-    # single_run(scrapy_config.scjst_thread)
-    # thread_run(scrapy_config.scjst_thread)
-    # single_run(scrapy_config.test_conf)
-    # thread_run(scrapy_config.test_conf)
+    try:
+        import scrapy_config
+        from base.command import build_run
 
-    # single_run(scrapy_config.re_scjst_base)
-    # thread_run(scrapy_config.re_scjst_base)
-
-    # single_run(scrapy_config.query_scjst_xmgk)
-    # thread_run(scrapy_config.query_scjst_xmgk)
-
-    single_run(scrapy_config.upwork_1)
-
+    except Exception as e:
+        print(e)
+        print("import error")
+    build_run(scrapy_config, sys.argv)

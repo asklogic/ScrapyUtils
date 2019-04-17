a4574389f2997b72a1367b0e9690cf4ba33fd84f
Sta1ker <logic.lj@outlook.com>
2019-04-17 23:52:38 +0800

    <chore> delet file

--- trigger.py
+++ trigger.py
@@ -1,11 +1,5 @@
 import sys
-from base.command import build_run
+from base.command import cli
 
 if __name__ == '__main__':
-    try:
-        # import scrapy_config
-        pass
-    except Exception as e:
-        print(e)
-        print("import error")
-    build_run(None, sys.argv)
+    cli()

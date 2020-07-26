fad02cb61e7ab8140501a36dff53e008110a71e3
logic <logic.lj@outlook.com>
2020-07-26 23:06:54 +0800

    <Fix>

--- trigger.py
+++ trigger.py
@@ -1,8 +1,8 @@
 import sys
 import os
-from base.command.commands import cli
+from ScrapyUtils.command.commands import cli
 
-sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
     cli()
